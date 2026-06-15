59a12097e9cb5ede54d0616ee362bdb45d3ec0ca
João Pedro Lopes Lenharo <joao.pedro.lopes.lenharo@gmail.com>
2026-06-15 12:19:52 -0300

    Refatorando HelloController para usar ResponseEntity

--- src/main/java/med/voll/api/controller/HelloController.java
+++ src/main/java/med/voll/api/controller/HelloController.java
@@ -1,5 +1,6 @@
 package med.voll.api.controller;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
 
     @GetMapping
-    public String olaMundo() {
-        return "Hello World Spring!";
+    public ResponseEntity<String> olaMundo() {
+        return ResponseEntity.ok("Hello World Spring!");
     }
 }
